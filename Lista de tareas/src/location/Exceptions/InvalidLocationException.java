474795f68841266356ef276e4b834e782f843eff
BrocardoSofia <sofia.brocardo@hotmail.com>
2023-06-12 19:43:26 -0300

    uptate

---
+++ Lista de tareas/src/location/Exceptions/InvalidLocationException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package location.Exceptions;
+
+/**
+ *
+ * @author sofia
+ */
+public class InvalidLocationException extends Exception{
+
+    public InvalidLocationException (String message) {
+        super(message);
+    }
+}
--- Lista de tareas/src/location/Location.java
+++ Lista de tareas/src/location/Location.java
@@ -6,12 +6,16 @@ package location;
 
 import APIController.APIController;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import location.Exceptions.InvalidLocationException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- *
- * @author sofia
+ *<h1>Clase Location</h1>
+ * Almacena la latitud y longitud, junto con el nombre de una localizacion
+ * 
+ * @author Sofia Brocardo
  */
 public class Location 
 {
@@ -19,6 +23,12 @@ public class Location
     private double lon;
     private String name;
     
+    
+    /**
+     * <h1>Constructor vacio</h1>
+     * 
+     * @author Sofia Brocardo
+     */
     public Location()
     {
         lat = 0;
@@ -26,6 +36,13 @@ public class Location
         name = "";
     }
     
+    /**
+     * <h1>Funcion que determina si tiene datos</h1>
+     * 
+     * @return boolean | true si esta vacio | false si tiene datos
+     * 
+     * @author Sofia Brocardo
+     */
     public boolean isEmpty()
     {
         boolean empty = false;
@@ -36,9 +53,25 @@ public class Location
         return empty;
     }
     
-    //sobrecargar la funcion updateLocation    
-    
-    public void updateLocation(String city, String state_province, String country) throws IOException
+        
+    /**
+     * <h1>Actualizar Localizacion</h1>
+     * Obtiene las coordenadas y el nombre del lugar de una API
+     * 
+     * @param city | ciudad, esta se puede especificar con un barrio separado por una coma, ej: barrio,ciudad
+     * @param state_province | estado o provincia
+     * @param country | pais
+     * 
+     * @see <a href="https://geocode.maps.co">Free Geocoding API</a> 
+     * @see APIController#getInfo(java.lang.String) 
+     * 
+     * @throws IOException | si algo sale mal con la API
+     * @throws MalformedURLException | si el URL es invalido
+     * @throws InvalidLocationException | si no se encuentra ninguna localizacion con ese nombre
+     * 
+     * @author Sofia Brocardo
+     */
+    public void updateLocation(String city, String state_province, String country) throws MalformedURLException, IOException, InvalidLocationException
     {
         city.replaceAll(" ","%20" );
         state_province.replaceAll(" ","%20" );
@@ -47,9 +80,23 @@ public class Location
         
         JSONArray jsonArray = new JSONArray(APIController.getInfo(urlAPI));
         
+        if(jsonArray.isEmpty())
+        {
+            //si el jsonArray esta vacio significa que la localizacion no existe
+            throw new InvalidLocationException("Localizacion invalida");
+        }
+        
         updateWithJSON(jsonArray);
     }
     
+    /**
+     * <h1>Actualizar datos con JSON</h1>
+     * Siempre guarda solo los datos del primer elemento del array
+     * 
+     * @param jsonArray | JSONArray con los datos de la localizacion
+     * 
+     * @author Sofia Brocardo
+     */
     private void updateWithJSON(JSONArray jsonArray)
     {
         JSONObject jsonObject = jsonArray.getJSONObject(0);
@@ -58,16 +105,37 @@ public class Location
         name = jsonObject.getString("display_name");
     }
     
+    /**
+     * <h1>Obtener Latitud</h1>
+     * 
+     * @return latitud
+     * 
+     * @author Sofia Brocardo
+     */
     public double getLat()
     {
         return lat;
     }
     
+    /**
+     * <h1>Obtener Longitud</h1>
+     * 
+     * @return longitud
+     * 
+     * @author Sofia Brocardo
+     */
     public double getLon()
     {
         return lon;
     }
     
+    /**
+     * <h1>Obtener nombre</h1>
+     * 
+     * @return nombre de la localizacion
+     * 
+     * @author Sofia Brocardo
+     */
     public String getName()
     {
         return name;
